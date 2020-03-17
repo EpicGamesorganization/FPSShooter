80f86ab7b5e513e39c67245eb9c3225c0bbec8c6
Dee <deegierke@gmail.com>
2020-03-17 20:22:16 +0200

    Plugins, Source, Main Files

---
+++ README.md
@@ -0,0 +1,3 @@
+# FPSShooter
+
+Developed with Unreal Engine 4
---
+++ Source/FPSShooter/SteamMultiplayer.h
@@ -0,0 +1,21 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "CoreMinimal.h"
+#include "Engine.h"
+#include "Net/UnrealNetwork.h"
+#include "UnrealNetwork.h"
+#include "Networking.h"
+#include "Online.h"
+
+/**
+ * 
+ */
+class FPSSHOOTER_API SteamMultiplayer
+{
+public:
+	SteamMultiplayer();
+	~SteamMultiplayer();
+};
